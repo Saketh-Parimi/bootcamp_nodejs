3c3f6905fe12ed72f8c2ae5b700ec71789b865b9
Saketh Parimi <sakethparimi@Sakeths-MacBook-Pro.local>
2021-07-14 14:12:47 -0700

    finished CRUD operations for bootcamps in db

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 const express = require("express");
 const dotenv = require("dotenv");
-
-// Router files
-const bootcamps = require('./routes/bootcamps');
 const morgan = require('morgan');
+
+// Created Files
+const connectDB = require('./config/db');
 
 // load env vars
 dotenv.config({ path: "./config/config.env" });
 
+// Connect to database
+connectDB();
+
+// Router files
+const bootcamps = require('./routes/bootcamps');
+
 const app = express();
+
+// Body parser
+app.use(express.json());
 
 // Dev logging middleware
 if (process.env.NODE_ENV === 'development') {
@@ -20,8 +29,14 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
-  // @ts-ignore
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
+
+// Handle unhandled rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  // Close server & exit proccess
+  server.close(() => process.exit(1));
+});

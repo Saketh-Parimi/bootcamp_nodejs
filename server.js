1470adeba2eb33dc9d7eb9069a140fc024af70dc
Saketh Parimi <sakethparimi@Sakeths-MacBook-Pro.local>
2021-07-14 11:36:03 -0700

    Added CRUD in controllers

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
 const express = require("express");
 const dotenv = require("dotenv");
 
+// Router files
+const bootcamps = require('./routes/bootcamps');
+const morgan = require('morgan');
+
 // load env vars
 dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// Mount routers
+app.use('/api/v1/bootcamps', bootcamps);
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
+  // @ts-ignore
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
